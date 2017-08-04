b4100598936a6a9ca8d901407aee50ed5c8193b6
srajivece <30692357+srajivece@users.noreply.github.com>
2017-08-04 21:43:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/article-one', function (req,res){
+    res.sen('articleone request served here');
+});
+app.get('/article-two', function (req,res){
+    res.sen('articleone request served here');
+});
+app.get('/article-three', function (req,res){
+    res.sen('articlthree request served here');
+});
+    
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
