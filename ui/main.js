11095474a2a0ca4bba9f60c2e51e8f77ef69e91c
srajivece <30692357+srajivece@users.noreply.github.com>
2017-08-11 20:09:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ element.innerHTML= 'New Value';*/
 
  var img =document.getElementById('madi');
  img.onclick = function() {
-     img.style.marginleft='100px';
+     img.style.marginLeft='100px';
  };
 
  //counter code
